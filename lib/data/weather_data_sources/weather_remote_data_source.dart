1db297ee4dad77b70f174fac6afe876a6aecd455
Pawel Kwiatkowski <pawelkwiatkowski81@gmail.com>
2023-10-16 19:37:57 +0200

    Add Dio handling error

--- lib/data/weather_data_sources/weather_remote_data_source.dart
+++ lib/data/weather_data_sources/weather_remote_data_source.dart
@@ -4,8 +4,13 @@ class WeatherRemoteDataSource {
   Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
-    final response = await Dio().get<Map<String, dynamic>>(
-        'http://api.weatherapi.com/v1/current.json?key=d2be5597e9294cc8b85164204232808&q=$city&aqi=no');
-    return response.data;
+    try {
+      final response = await Dio().get<Map<String, dynamic>>(
+          'http://api.weatherapi.com/v1/current.json?key=d2be5597e9294cc8b85164204232808&q=$city&aqi=no');
+      return response.data;
+    } on DioError catch (error) {
+      throw Exception(
+          error.response?.data['error']['message'] ?? 'Unknown error');
+    }
   }
 }
